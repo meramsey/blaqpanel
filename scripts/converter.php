571fc899366795d4600b66649c5fb1659985e725
Michael Ramsey <mike@hackerdise.me>
2021-09-26 17:17:00 -0400

    Converted ols plaintext to lsws xml configs

---
+++ scripts/converter.php
@@ -0,0 +1,17 @@
+<?php
+// ols config to lsws xml converter
+// ${LS_DIR}/admin/misc/converter.php
+// place in /usr/local/lsws/admin/misc/converter.php
+// wget -O /usr/local/lsws/admin/misc/converter.php
+$lsws = dirname(dirname(__DIR__)) . '/';
+ini_set(
+	'include_path',
+	$lsws . 'admin/html/lib/:' .
+		$lsws . 'admin/html/lib/ows/:' .
+		$lsws . 'admin/html/view/'
+);
+date_default_timezone_set('America/New_York');
+spl_autoload_register( function ($class) {
+	include $class . '.php';
+});
+CData::Util_Migrate_AllConf2Xml($lsws);
